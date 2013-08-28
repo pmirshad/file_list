f0870085689add7750edb9b18736e49fded5244d
Irshad Pananilath <pmirshad+code@gmail.com>
2013-08-29 02:56:13 +0530

    modify formatting to use spaces instead of tabs (gofmt -w -tabs=false -tabwidth=2)

--- file_list.go
+++ file_list.go
@@ -8,51 +8,51 @@
 package file_list
 
 import (
-	"os"
-	"path/filepath"
-	"time"
+  "os"
+  "path/filepath"
+  "time"
 )
 
 type FileInfo struct {
-	AbsPath string      // absolute path for the file
-	Name    string      // base name of the file
-	Size    int64       // length in bytes for regular files
-	Mode    os.FileMode // file mode bits
-	ModTime time.Time   // modification time
-	IsDir   bool        // abbreviation for Mode().IsDir()
+  AbsPath string      // absolute path for the file
+  Name    string      // base name of the file
+  Size    int64       // length in bytes for regular files
+  Mode    os.FileMode // file mode bits
+  ModTime time.Time   // modification time
+  IsDir   bool        // abbreviation for Mode().IsDir()
 }
 
 // AllFiles enumerates a list of files (and directories) under the directory
 // specified as argument. Directories are included if include_dirs is set
 // to true. The function returns a slice of type FileInfo
 func AllFiles(root string, include_dirs bool) (result []FileInfo) {
-	files := []FileInfo{}
+  files := []FileInfo{}
 
-	absRoot, err := filepath.Abs(root)
+  absRoot, err := filepath.Abs(root)
 
-	if err != nil {
-		return
-	}
+  if err != nil {
+    return
+  }
 
-	walkFunc := func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if include_dirs || !info.IsDir() {
-			files = append(files, FileInfo{
-				path,
-				info.Name(),
-				info.Size(),
-				info.Mode(),
-				info.ModTime(),
-				info.IsDir()})
-		}
-		return nil
-	}
+  walkFunc := func(path string, info os.FileInfo, err error) error {
+    if err != nil {
+      return err
+    }
+    if include_dirs || !info.IsDir() {
+      files = append(files, FileInfo{
+        path,
+        info.Name(),
+        info.Size(),
+        info.Mode(),
+        info.ModTime(),
+        info.IsDir()})
+    }
+    return nil
+  }
 
-	if err := filepath.Walk(absRoot, walkFunc); err != nil {
-		return
-	}
+  if err := filepath.Walk(absRoot, walkFunc); err != nil {
+    return
+  }
 
-	return files
+  return files
 }
